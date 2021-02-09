8bdf22c8315234eaa761a5c6433cadb2177bd074
Денис <den.khabibullin@gmail.com>
2021-02-09 16:19:53 +0300

    Revert "Revert "Загружены все эффекты, которые я делал последние месяцы""
    This reverts commit cf867b62de14f67b0c509b9567dc2161fe339ac4.

---
+++ Assets/Script/Effects/MoveToMouse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+
+public class MoveToMouse : MonoBehaviour
+{
+
+    public float Speed = 8.0f;
+    public float DistanceFromCamera = 5.0f;
+
+    void Update()
+    {
+        Vector3 MousePosition = Input.mousePosition;
+        MousePosition.z = DistanceFromCamera;
+
+        Vector3 MouseScreenToWorld = Camera.main.ScreenToWorldPoint(MousePosition);
+
+        Vector3 Position = Vector3.Lerp(transform.position, MouseScreenToWorld, 1.0f - Mathf.Exp(-Speed * Time.deltaTime));
+
+        transform.position = Position;
+    }
+}
